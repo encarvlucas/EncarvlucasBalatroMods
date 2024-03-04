3c09409e26ed95727acfbba09ec2c24a1f2fcfd8
Lucas Carvalho de Sousa <30300926+encarvlucas@users.noreply.github.com>
2024-03-03 22:46:58 -0300

    Initial commit

---
+++ EnchantedDeck.lua
@@ -0,0 +1,88 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Enchanted Deck
+--- MOD_ID: EnchantedDeck
+--- MOD_AUTHOR: [Encarvlucas]
+--- MOD_DESCRIPTION: Start the game with some of your cards already be enhanced!
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function randomSelect(table)
+	if #table == 0 then
+			return nil -- Table is empty
+	end
+	local randomIndex = math.random(1, #table)
+	return table[randomIndex]
+end
+
+local Backapply_to_runRef = Back.apply_to_run
+function Back.apply_to_run(arg_56_0)
+	Backapply_to_runRef(arg_56_0)
+
+	if arg_56_0.effect.config.enchanted then
+		G.E_MANAGER:add_event(Event({
+			func = function()
+				local trandom_m = {
+					G.P_CENTERS.m_stone,
+					G.P_CENTERS.m_steel,
+					G.P_CENTERS.m_glass,
+					G.P_CENTERS.m_gold,
+					G.P_CENTERS.m_bonus,
+					G.P_CENTERS.m_mult,
+					G.P_CENTERS.m_wild,
+					G.P_CENTERS.m_lucky,
+				}
+				local trandom_e = {
+					{foil = true},
+					{holo = true},
+					{polychrome = true},
+				}
+				local trandom_g = {
+					"Red",
+					"Blue",
+					"Gold",
+					"Purple",
+				}
+				for iter_57_0 = #G.playing_cards, 1, -1 do
+					-- Chance to apply modifications 
+					-- 20% to have one of the enhancements
+					if math.random(100) <= 20 then  
+						local random_m = randomSelect(trandom_m)
+						G.playing_cards[iter_57_0]:set_ability(random_m)
+					end
+
+					-- 7.5% to have one of the editions
+					if math.random(100) <= 8 then  
+						local random_e = randomSelect(trandom_e)
+						G.playing_cards[iter_57_0]:set_edition(random_e, true, true)
+					end
+
+					-- 7.5% to have one of the seals
+					if math.random(100) <= 8 then  
+						local random_g = randomSelect(trandom_g)
+						G.playing_cards[iter_57_0]:set_seal(random_g, true, true)
+					end
+				end
+
+				return true
+			end
+		}))
+	end
+end
+
+function SMODS.INIT.EnchantedDeck()
+	local loc_def = {
+		["name"]="Enchanted Deck",
+		["text"]={
+			[1]="Start with a Deck",
+			[2]="with some cards",
+			[3]="already {C:attention}Enhanced{}"
+		},
+	}
+
+	local absolute = SMODS.Deck:new("Enchanted Deck", "enchanted", {enchanted = true}, {x = 5, y = 2}, loc_def)
+	absolute:register()
+end
+
+----------------------------------------------
+------------MOD CODE END----------------------
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# EncarvlucasBalatroMods
-Repository to track my Balatro modding projects
+# Encarvlucas Balatro Mods
+Mods I made for the awesome game [Balatro](https://store.steampowered.com/app/2379780/Balatro/).
+
+## Enchanted Deck
+Start the game with some of your cards already be enhanced!
+
+Odds are (check mod files as these are subject to change):
+- 20% that a card is **_enhanced_**
+- 8% that a card has a **_special edition_**
+- 8% that a card has a **_seal_**
+
+
+## Installation
+- This mod requires [ModLoader](https://github.com/Steamopollys/Steamodded/).
+- Just the download the `.lua` files of the mods you want to install and place them in your `C:\Users\<USER>\AppData\Roaming\Balatro\Mods` or `%appdata%\Balatro\Mods` directory.
+
+
+## Known Issues
+- None so far
+
+## Contributing
+Pull requests are welcome.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
